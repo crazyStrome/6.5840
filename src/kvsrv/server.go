e500db2bdad6c17f523c4df4d3ed3397b3ea8780
haandy <1415441419@qq.com>
2024-03-09 10:21:37 +0800

    更新逻辑

--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -15,18 +15,18 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 }
 
 type KVServer struct {
+	mu sync.Mutex
+
 	// Your definitions here.
-	data       sync.Map
+	data       map[string]string
 	requestIDs sync.Map
 }
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
-	v, ok := kv.data.Load(args.Key)
-	if !ok {
-		return
-	}
-	reply.Value = v.(string)
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	reply.Value = kv.data[args.Key]
 }
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
@@ -42,15 +42,16 @@ func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
 		return
 	}
 
-	v, ok = kv.data.Load(args.Key)
-	if ok {
-		reply.Value = v.(string)
+	kv.mu.Lock()
+	old := kv.data[args.Key]
+	if old != args.Value {
+		kv.data[args.Key] = args.Value
 	}
-	if reply.Value != args.Value {
-		kv.data.Store(args.Key, args.Value)
-	}
+	kv.mu.Unlock()
 
-	kv.requestIDs.Store(args.RequestID, reply.Value)
+	reply.Value = old
+
+	kv.requestIDs.Store(args.RequestID, old)
 }
 
 func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
@@ -66,21 +67,25 @@ func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
 		return
 	}
 
-	v, ok = kv.data.Load(args.Key)
-	if ok {
-		reply.Value = v.(string)
-		kv.data.Store(args.Key, v.(string)+args.Value)
+	kv.mu.Lock()
+	old := kv.data[args.Key]
+	if old == "" {
+		kv.data[args.Key] = args.Value
 	} else {
-		kv.data.Store(args.Key, args.Value)
+		kv.data[args.Key] = old + args.Value
 	}
+	kv.mu.Unlock()
 
-	kv.requestIDs.Store(args.RequestID, reply.Value)
+	reply.Value = old
+
+	kv.requestIDs.Store(args.RequestID, old)
 }
 
 func StartKVServer() *KVServer {
 	kv := new(KVServer)
 
 	// You may need initialization code here.
+	kv.data = make(map[string]string, 20)
 
 	return kv
 }
