8be36456c557b8df18fbb97bab0138ccb8163a8e
haandy <1415441419@qq.com>
2024-04-07 20:27:00 +0800

    完成单测3a

--- src/raft/util.go
+++ src/raft/util.go
@@ -5,6 +5,10 @@ import "log"
 // Debugging
 const Debug = false
 
+func init() {
+	log.SetFlags(log.Lmicroseconds)
+}
+
 func DPrintf(format string, a ...interface{}) {
 	if Debug {
 		log.Printf(format, a...)
